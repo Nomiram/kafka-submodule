2e76efa79e6f012dc65dd056e3a336e0ad52b10d
Andrey Mironenko <qwerty.mironenko@gmail.com>
2022-11-10 19:15:01 +0300

    update consumer

--- kafka-consumer.go
+++ kafka-consumer.go
@@ -1,39 +1,26 @@
 package handlers
 
 import (
-	"context"
 	"fmt"
 	"time"
 
 	"github.com/segmentio/kafka-go"
 )
 
-func KafkaConsumer() *kafka.Conn {
+func KafkaConsumer() *kafka.Reader {
 	fmt.Println("msg from Consumer")
 
 	// to consume messages
 	topic := "my-topic-1"
 	partition := 0
-	var conn *kafka.Conn
-	var err error
-	// time.Sleep(time.Second * 5)
-	for {
-		// kafka.DialLeader()
-		conn, err = kafka.DialLeader(context.Background(), "tcp", "kafka_0:9092", topic, partition)
-		if err != nil {
-			//
-			fmt.Println("Comsumer: failed to dial leader:", err)
-			time.Sleep(time.Second * 2)
-			continue
-		}
-		break
-	}
-
-	// conn.SetReadDeadline(time.Now().Add(10 * time.Second))
-
-	// if err := conn.Close(); err != nil {
-	// 	log.Fatal("failed to close connection:", err)
-	// }
+	var conn *kafka.Reader
+	brokerAddress := []string{"kafka_0:9092", "kafka_1:9093", "kafka_2:9094"}
+	conn = kafka.NewReader(kafka.ReaderConfig{
+		Brokers: brokerAddress,
+		Topic:   topic,
+		// GroupID: "group-rating",
+		Partition: partition,
+	})
 	return conn
 
 }
